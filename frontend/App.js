a024aa9bc39abc7b89925853f72daa8c00763a99
botoysigma <sigmawooddog@gmail.com>
2025-11-09 17:42:54 +0530

    Update App.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,1 +1,13 @@
+import React from "react";
+import { SafeAreaView, StatusBar } from "react-native";
+import HomeScreen from "./src/screens/HomeScreen";
+
+export default function App() {
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
+      <StatusBar barStyle="dark-content" />
+      <HomeScreen />
+    </SafeAreaView>
+  );
+}
 
