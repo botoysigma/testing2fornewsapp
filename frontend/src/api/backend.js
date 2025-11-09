1440b83e83ed53b64ef13e48612258961f136d46
botoysigma <sigmawooddog@gmail.com>
2025-11-09 17:43:17 +0530

    Update backend.js

--- frontend/src/api/backend.js
+++ frontend/src/api/backend.js
@@ -1,1 +1,13 @@
+import axios from "axios";
+import { BACKEND_URL } from "@env";
+
+const api = axios.create({
+  baseURL: BACKEND_URL || "http://localhost:4000",
+  timeout: 10000,
+});
+
+export async function fetchNews(category = "general") {
+  const res = await api.get(`/news?country=in&category=${category}`);
+  return res.data.articles;
+}
 
